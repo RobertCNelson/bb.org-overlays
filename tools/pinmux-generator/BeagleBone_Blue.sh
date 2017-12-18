120a90d98b450b1ad83383bdfa49394ed6b5d7d5
Robert Nelson <robertcnelson@gmail.com>
2017-12-18 13:29:23 -0600

    pinmux: BeagleBone_Blue: add gpio_input
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/pinmux-generator/BeagleBone_Blue.sh
+++ tools/pinmux-generator/BeagleBone_Blue.sh
@@ -6,95 +6,128 @@ source $(dirname "$0")/lib/pinmux.sh
 
 file="BeagleBone_Blue"
 
-unset generate_timer
-
 echo "" > ${file}.dts
 echo "" >${file}-pinmux.dts
 echo "" >${file}-gpio.dts
 echo "" >${file}_config-pin.txt
 
+disable_timer="enable"
+
 #BeagleBone Blue
+gpio_index="7"
 
 #E1
 #EQEP_0A
-cp_info_default="eqep0a_in" ; cp_default="qep" ; default="qep" ; pcbpin="E1_3" ; ball="B12" ; find_ball
+pcbpin="E1_3" ; ball="B12" ; default_mode="1" ; cp_default="qep" ; find_ball
 #EQEP_0B
-cp_info_default="eqep0b_in" ; cp_default="qep" ; default="qep" ; pcbpin="E1_4" ; ball="C13" ; find_ball
+pcbpin="E1_4" ; ball="C13" ; default_mode="1" ; cp_default="qep" ; find_ball
 
 #E2
 #EQEP_1A V2
-cp_info_default="eqep1a_in" ; cp_default="qep" ; default="qep" ; pcbpin="E2_3" ; ball="V2" ; find_ball
+gpio_index="6"
+pcbpin="E2_3" ; ball="V2" ; default_mode="2" ; cp_default="qep" ; find_ball
+gpio_index="7"
+
 #EQEP_1B V3
-cp_info_default="eqep1b_in" ; cp_default="qep" ; default="qep" ; pcbpin="E2_4" ; ball="V3" ; find_ball
+gpio_index="6"
+pcbpin="E2_4" ; ball="V3" ; default_mode="2" ; cp_default="qep" ; find_ball
+gpio_index="7"
 
 #E3
 #EQEP_1A T12
-cp_info_default="eqep2a_in" ; cp_default="qep" ; default="qep" ; pcbpin="E3_3" ; ball="T12" ; find_ball
+pcbpin="E3_3" ; ball="T12" ; default_mode="4" ; cp_default="qep" ; find_ball
 #EQEP_1B R12
-cp_info_default="eqep2b_in" ; cp_default="qep" ; default="qep" ; pcbpin="E3_4" ; ball="R12" ; find_ball
+pcbpin="E3_4" ; ball="R12" ; default_mode="4" ; cp_default="qep" ; find_ball
 
 #E4
 #pruin V13
-cp_info_default="pruin" ; cp_default="pruin" ; default="pruin" ; pcbpin="E4_3" ; ball="V13" ; find_ball
+pcbpin="E4_3" ; ball="V13" ; default_mode="6" ; cp_default="pruin" ; find_ball
 #pruin U13
-cp_info_default="pruin" ; cp_default="pruin" ; default="pruin" ; pcbpin="E4_4" ; ball="U13" ; find_ball
+pcbpin="E4_4" ; ball="U13" ; default_mode="6" ; cp_default="pruin" ; find_ball
 
 #UT0
 #UART0_RX
-cp_info_default="uart" ; cp_default="uart" ; default="UART" ; pcbpin="UT0_3" ; ball="E15" ; find_ball
+pcbpin="UT0_3" ; ball="E15" ; default_mode="0" ; cp_default="uart" ; find_ball
 #UART0_TX
-cp_info_default="uart" ; cp_default="uart" ; default="UART" ; pcbpin="UT0_4" ; ball="E16" ; find_ball
+pcbpin="UT0_4" ; ball="E16" ; default_mode="0" ; cp_default="uart" ; find_ball
 
 #UT1
 #UART1_RX
-cp_info_default="uart" ; cp_default="uart" ; default="UART" ; pcbpin="UT1_3" ; ball="D16" ; find_ball
+gpio_index="6"
+pcbpin="UT1_3" ; ball="D16" ; default_mode="0" ; cp_default="uart" ; find_ball
+gpio_index="7"
+
 #UART1_TX
-cp_info_default="uart" ; cp_default="uart" ; default="UART" ; pcbpin="UT1_4" ; ball="D15" ; find_ball
+gpio_index="6"
+pcbpin="UT1_4" ; ball="D15" ; default_mode="0" ; cp_default="uart" ; find_ball
+gpio_index="7"
 
 #UT5
 #UART5_RX
-cp_info_default="uart" ; cp_default="uart" ; default="UART" ; pcbpin="UT5_3" ; ball="U2" ; find_ball
+pcbpin="UT5_3" ; ball="U2" ; default_mode="4" ; cp_default="uart" ; find_ball
 #UART5_TX
-cp_info_default="uart" ; cp_default="uart" ; default="UART" ; pcbpin="UT5_4" ; ball="U1" ; find_ball
+pcbpin="UT5_4" ; ball="U1" ; default_mode="4" ; cp_default="uart" ; find_ball
 
 #CAN has to be on...
 
 #DSM2
 #UART4_RX
-cp_info_default="uart" ; cp_default="uart" ; default="UART" ; pcbpin="DSM2_3" ; ball="T17" ; find_ball
+pcbpin="DSM2_3" ; ball="T17" ; default_mode="6" ; cp_default="uart" ; find_ball
 
 #i2c has to be on...
 
 #GP0
-pcbpin="GP0_3" ; ball="U16" ; find_ball
-pcbpin="GP0_4" ; ball="V14" ; find_ball
-pcbpin="GP0_5" ; ball="D13" ; find_ball
-pcbpin="GP0_6" ; ball="C12" ; find_ball
+gpio_index="6"
+pcbpin="GP0_3" ; ball="U16" ; default_mode="7" ; find_ball
+gpio_index="7"
+
+pcbpin="GP0_4" ; ball="V14" ; default_mode="7" ; find_ball
+
+gpio_index="6"
+pcbpin="GP0_5" ; ball="D13" ; default_mode="7" ; find_ball
+gpio_index="7"
+
+pcbpin="GP0_6" ; ball="C12" ; default_mode="7" ; find_ball
 
 #GP1
-pcbpin="GP1_3" ; ball="J15" ; find_ball
-pcbpin="GP1_4" ; ball="H17" ; find_ball
-pcbpin="GP1_5" ; ball="R7" ; find_ball
-pcbpin="GP1_6" ; ball="T7" ; find_ball
+pcbpin="GP1_3" ; ball="J15" ; default_mode="7" ; find_ball
+
+pcbpin="GP1_4" ; ball="H17" ; default_mode="7" ; find_ball
+
+gpio_index="2"
+pcbpin="GP1_5" ; ball="R7" ; default_mode="7" ; find_ball
+gpio_index="7"
+
+gpio_index="2"
+pcbpin="GP1_6" ; ball="T7" ; default_mode="7" ; find_ball
+gpio_index="7"
 
 #ADC
 
 #GPS
-cp_info_default="uart" ; cp_default="uart" ; default="UART" ; pcbpin="GPS_3" ; ball="A17" ; find_ball
-cp_info_default="uart" ; cp_default="uart" ; default="UART" ; pcbpin="GPS_4" ; ball="B17" ; find_ball
+pcbpin="GPS_3" ; ball="A17" ; default_mode="1" ; cp_default="uart" ; find_ball
+pcbpin="GPS_4" ; ball="B17" ; default_mode="1" ; cp_default="uart" ; find_ball
 
 #SPI1
 #SPI1_MOSI
-cp_info_default="spi1_d1" ; cp_default="spi" ; default="SPI" ; pcbpin="SPI1_3" ; ball="D12" ; find_ball
+gpio_index="6"
+pcbpin="SPI1_3" ; ball="D12" ; default_mode="3" ; cp_default="spi" ; find_ball
+gpio_index="7"
+
 #SPI1_MISO
-cp_info_default="spi1_d0" ; cp_default="spi" ; default="SPI" ; pcbpin="SPI1_4" ; ball="B13" ; find_ball
+gpio_index="6"
+pcbpin="SPI1_4" ; ball="B13" ; default_mode="3" ; cp_default="spi" ; find_ball
+gpio_index="7"
+
 #SPI1_SCK
-cp_info_default="spi1_sclk" ; cp_default="spi_sclk" ; default="spi_sclk" ; pcbpin="SPI1_5" ; ball="A13" ; find_ball
+gpio_index="6"
+pcbpin="SPI1_5" ; ball="A13" ; default_mode="3" ; cp_default="spi_sclk" ; find_ball
+gpio_index="7"
 
 #SPI1_SS1
-cp_info_default="spi1_cs0" ; cp_default="spi_cs" ; default="spi_cs" ; pcbpin="S1.1_6" ; ball="H18" ; find_ball
+pcbpin="S1.1_6" ; ball="H18" ; default_mode="2" ; cp_default="spi_cs" ; find_ball
 #SPI1_SS2
-cp_info_default="spi1_cs1" ; cp_default="spi_cs" ; default="spi_cs" ; pcbpin="S1.2_6" ; ball="C18" ; find_ball
+pcbpin="S1.2_6" ; ball="C18" ; default_mode="2" ; cp_default="spi_cs" ; find_ball
 
 msg="" ; echo_both
 
