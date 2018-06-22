14e0840b6050243de55f54d50e03bf1269f78c7a
Robert Nelson <robertcnelson@gmail.com>
2018-06-22 15:00:30 -0500

    Jenkins: remove old branch
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -20,8 +20,13 @@ make clean
 git checkout master -f
 git pull || true
 
+test_for_branch=$(git branch --list ${git_tag}-build)
+if [ "x${test_for_branch}" != "x" ] ; then
+	git branch ${git_tag}-build -D
+fi
+
 git checkout ${git_tag} -b ${git_tag}-build
 
 make clean
 make CC=gcc CROSS_COMPILE= all
-echo "dtc: `/var/lib/jenkins/git/dtc --version`"
+echo "dtc: `/var/lib/jenkins/git/dtc/dtc --version`"
