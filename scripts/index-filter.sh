5e8094d28c90060bb920211e280b3b83616317b3
Robert Nelson <robertcnelson@gmail.com>
2019-05-07 08:52:24 -0500

    license: this was a fork of https://git.kernel.org/pub/scm/linux/kernel/git/devicetree/devicetree-rebasing.git/ remerge upstream changes
    fixes: https://github.com/beagleboard/bb.org-overlays/issues/118
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/index-filter.sh
+++ scripts/index-filter.sh
@@ -3,8 +3,7 @@
 set -e
 set -o pipefail
 
-git ls-files -s | tee /tmp/bar | sed -n -f ${SCRIPTS}/rewrite-paths.sed | tee /tmp/foo | \
-	GIT_INDEX_FILE=$GIT_INDEX_FILE.new git update-index --index-info
+${SCRIPTS}/rewrite-index.pl | GIT_INDEX_FILE=$GIT_INDEX_FILE.new git update-index --index-info
 
 if [ -f "$GIT_INDEX_FILE.new" ] ; then
     mv "$GIT_INDEX_FILE.new" "$GIT_INDEX_FILE"
